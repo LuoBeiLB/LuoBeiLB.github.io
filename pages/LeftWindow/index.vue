23e7b00ed017ff7bff46c42f51b335e0a0380c02
LuoBeiLB <869963554@qq.com>
2023-06-17 21:28:21 +0800

    第二次

---
+++ pages/LeftWindow/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view>
+			这是左边栏
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+		},
+		onLoad() {
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view>
+			这是内容
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
